a1bb1345f0122283fe15f5648b7886500daf2aa4
Nextias <accnext1997@gmail.com>
2023-09-29 13:23:07 +0300

    add tests

--- mb_project/posts/tests.py
+++ mb_project/posts/tests.py
@@ -1,12 +1,7 @@
 # pages/tests.py
-from django.test import SimpleTestCase, TestCase
+from django.test import TestCase
+from django.urls import reverse
 from .models import Post
-
-
-class SimpleTests(TestCase):
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
 
 
 class PostModelTest(TestCase):
@@ -17,6 +12,24 @@ class PostModelTest(TestCase):
         post = Post.objects.get(id=1)
         expected_object_name = f'{post.text}'
         self.assertEqual(expected_object_name, 'just a test')
+
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='just a test')
+
+    def test_home_page_at_proper_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
 
 
 
