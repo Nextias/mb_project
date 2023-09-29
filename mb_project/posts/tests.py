beba2d4ff017a3e9cca4975779a1c66a10221753
Nextias <accnext1997@gmail.com>
2023-09-29 13:13:40 +0300

    add tests

--- mb_project/posts/tests.py
+++ mb_project/posts/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+# pages/tests.py
+from django.test import SimpleTestCase, TestCase
+from .models import Post
 
-# Create your tests here.
+
+class SimpleTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='just a test')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'just a test')
+
+
+
